df99d1ee1f11478a2292ee43444970b2d856281a
z6nc <andresvelasquezarotinco@gmail.com>
2025-08-05 17:29:16 -0500

    fix: format code for consistency and readability

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,14 +2,17 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from "@tailwindcss/vite";
 import react from "@astrojs/react";
-// https://astro.build/config
+
 export default defineConfig({
   vite: {
-      plugins: [tailwindcss()],
+    plugins: [tailwindcss()],
+    optimizeDeps: {
+      include: ['react', 'react-dom']
+    }
   },
 
   prefetch: {
-      prefetchAll: true
+    prefetchAll: true
   },
 
   integrations: [react()]
